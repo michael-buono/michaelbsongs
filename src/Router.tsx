ea58b6174c00319f81e9b4ede57ab56dbc79c526
Michael Buono <michaelbuono5@gmail.com>
2025-10-26 18:54:03 -0400

    Add Business Card

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,10 +1,12 @@
 import { HashRouter, Route, Routes } from 'react-router-dom'
+import BusinessCard from './pages/BusinessCard.page'
 import Home from './pages/Home.page'
 
 export function Router() {
   return (
     <HashRouter>
       <Routes>
+        <Route path="/business-card" element={<BusinessCard />} />
         {/* This wildcard ensures that any path (e.g. /watch, /about) renders Home */}
         <Route path="/*" element={<Home />} />
       </Routes>
